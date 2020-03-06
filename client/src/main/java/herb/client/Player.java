1ba99ddfc785036cfcecaf5c61e4b19b60ee1cc4
bilger-rafael <55615943+bilger-rafael@users.noreply.github.com>
2020-03-06 13:28:52 +0100

    Fix failed copy

---
+++ client/src/main/java/herb/client/Player.java
@@ -0,0 +1,30 @@
+package herb.client;
+
+public class Player {
+	private String name;
+	private Integer rank;
+	
+	public Player() { }
+	
+	public Player(String name, Integer rank) {
+		this.setName(name);
+		this.setRank(rank);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getRank() {
+		return rank;
+	}
+
+	public void setRank(Integer rank) {
+		this.rank = rank;
+	}
+
+}
