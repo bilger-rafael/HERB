e8338d7aab194164fd0af6c9e5e97fe64dcaa314
bodybilger <bodybilger@rafaels-mbp.edu.ds.fhnw.ch>
2020-03-07 12:04:55 +0100

    added package for rest services

--- server/src/main/java/herb/server/rest/PlayerControler.java
+++ server/src/main/java/herb/server/rest/PlayerControler.java
@@ -1,15 +1,18 @@
-package herb.server;
+package herb.server.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class PlayerControler {
 	
+	/*
 	@GetMapping("/player")
 	public Player player(@RequestParam(value = "name", defaultValue = "Rafael") String name) {
-		return new Player(name, 1);
+		return new Player();
 	}
+	*/
 	
 }
