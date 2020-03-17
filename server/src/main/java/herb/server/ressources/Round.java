2079aebebd09ebb060440cb7714cb9b65adc21a7
Etter <dominic.etter@students.fhnw.ch>
2020-03-17 07:16:09 +0100

    Idea of Trump created

--- server/src/main/java/herb/server/ressources/Round.java
+++ server/src/main/java/herb/server/ressources/Round.java
@@ -3,12 +3,30 @@ package herb.server.ressources;
 import herb.server.ressources.core.RoundBase;
 import herb.server.ressources.core.TrickBase;
 
+//Etter
 public class Round extends RoundBase{
+	Trump currentTrump;
+	
+	@Override
+	public Round() {
+		this.genTrump();
+	}
 
 	@Override
-	public TrickBase getCurrentTick() {
+	public TrickBase getCurrentTrick() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	@Override
+	private void genTrump() {
+		
+		
+	}
+	
+	@Override
+	public Trump getCurrentTrump() {
+		return currentTrump;
 	}
 
 }
--- server/src/main/java/herb/server/ressources/core/RoundBase.java
+++ server/src/main/java/herb/server/ressources/core/RoundBase.java
@@ -1,7 +1,15 @@
 package herb.server.ressources.core;
 
+//Etter
 public abstract class RoundBase {
 	private TrickBase[] tricks;
+	private Trump current Trump;
 	
-	public abstract TrickBase getCurrentTick();
+	public abstract RoundBase();
+	
+	public abstract TrickBase getCurrentTrick();
+	
+	private abstract void genTrump();
+	
+	public abstract Trump getCurrentTrump();
 }
