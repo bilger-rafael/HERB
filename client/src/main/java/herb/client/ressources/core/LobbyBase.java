cbe8234eee27e1760b0309b5333c5266ced446ae
bodybilger <bodybilger@localhost>
2020-03-22 15:30:44 +0100

    copied Base class from server to client

--- client/src/main/java/herb/client/ressources/core/LobbyBase.java
+++ client/src/main/java/herb/client/ressources/core/LobbyBase.java
@@ -6,22 +6,26 @@ public abstract class LobbyBase {
 	private final UUID uuid;
 	private final String name;
 	protected PlayerBase[] players = new PlayerBase[4];
-	
+
 	public LobbyBase(UUID uuid, String name) {
 		this.uuid = uuid;
 		this.name = name;
 		this.players = new PlayerBase[4];
 	}
-	
-	public abstract GameBase startGame();
 
 	public String getName() {
 		return name;
 	}
-	
-	//Etter
+
+	public PlayerBase[] getPlayers() {
+		return players;
+	}
+
+	public abstract GameBase startGame();
+
+	// Etter
 	public abstract void addPlayer(PlayerBase p);
-	
-	//Etter
+
+	// Etter
 	public abstract void removePlayer(PlayerBase p);
 }
