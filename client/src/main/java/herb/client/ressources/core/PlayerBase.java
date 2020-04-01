48cf318987f071d4938dc46c450b48bab8687890
bodybilger <bodybilger@localhost>
2020-04-01 19:23:01 +0200

    implemented equals method in PlayerBase

--- client/src/main/java/herb/client/ressources/core/PlayerBase.java
+++ client/src/main/java/herb/client/ressources/core/PlayerBase.java
@@ -7,35 +7,51 @@ public abstract class PlayerBase {
 	private final String authToken;
 	private RoundBase round;
 	protected HandBase hand;
-	
-	@JsonCreator	
+
+	@JsonCreator
 	public PlayerBase(String username, String authToken) {
 		this.username = username;
 		this.authToken = authToken;
 	}
-	
+
+	@Override
+	public boolean equals(Object o) {
+		// self check
+		if (this == o)
+			return true;
+		// null check
+		if (o == null)
+			return false;
+		// type check and cast
+		if (getClass() != o.getClass())
+			return false;
+		PlayerBase p = (PlayerBase) o;
+		// field comparison
+		return this.getUsername().equals(p.getUsername());
+	}
+
 	public abstract void play(CardBase card);
 
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public String getAuthToken() {
 		return authToken;
 	}
-	
+
 	public void setRound(RoundBase round) {
 		this.round = round;
 	}
 
 	public abstract void addCardtoHand(CardBase card);
-	
+
 	public abstract boolean PlayerNoCards();
-	
+
 	public abstract void clearHand();
-	
+
 	public abstract void sortHand();
-	
+
 	public abstract CardBase[] determinPlayableCards();
 
 	public RoundBase getRound() {
