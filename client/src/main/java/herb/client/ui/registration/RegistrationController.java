c2d98628d11b53b0ec2be033dea91b10f3fa339b
Anahí <Anah@DESKTOP-VVOSDRT.home>
2020-04-09 16:50:54 +0200

    update registration controller; getBackLoginView

--- client/src/main/java/herb/client/ui/registration/RegistrationController.java
+++ client/src/main/java/herb/client/ui/registration/RegistrationController.java
@@ -1,17 +1,39 @@
 package herb.client.ui.registration;
 
+import herb.client.utils.ServiceLocator;
 import herb.client.Main;
 import herb.client.ressources.core.ExceptionBase;
 import herb.client.ui.core.Controller;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.stage.WindowEvent;
 
 public class RegistrationController extends Controller<RegistrationModel, RegistrationView> {
-
+	ServiceLocator serviceLocator;
+		
 	public RegistrationController(RegistrationModel model, RegistrationView view) {
 		super(model, view);
 		
 		// Action for CreateUserButton
 		view.getRegistrationButton().setOnAction(e -> registration());
+		
+		/**
+		 * action for cancelButton
+		 * Herren
+		 */
+		view.getCancelButton().setOnAction(e -> getBackLoginView());
+		
+		view.getStage().setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				Platform.exit();
+			}
+	});
+		serviceLocator = ServiceLocator.getInstance();
+		serviceLocator.getLogger().info("Application controller initialized");
+	
 	}
+
 
 	private void registration() {
 		String username = view.getNameTextField().getText();
@@ -25,6 +47,13 @@ public class RegistrationController extends Controller<RegistrationModel, Regist
 			// TODO show error message
 		}
 
+	}
+	/**
+	 * 
+	 */
+	private void getBackLoginView() {
+		this.view.stop();
+		Main.getMainProgram().getLoginView().start();
 	}
 
 }
