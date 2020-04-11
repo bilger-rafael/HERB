a46a8fd40aff77f956202e68cf6c4534b485d5d2
bodybilger <bodybilger@localhost>
2020-04-11 14:25:58 +0200

    read lobbys into observable list

--- client/src/main/java/herb/client/ui/launcher/LauncherModel.java
+++ client/src/main/java/herb/client/ui/launcher/LauncherModel.java
@@ -1,11 +1,30 @@
 package herb.client.ui.launcher;
 
+import herb.client.ressources.Lobby;
+import herb.client.ressources.core.ExceptionBase;
 import herb.client.ui.core.Model;
 
 public class LauncherModel extends Model{
+	private Lobby[] lobbys;
 
     public LauncherModel() {
     	super();
+    	refreshLobbyList();
+    	
+    	//TODO refresh via button click or all 5 min
     }
+    
+    public void refreshLobbyList() {
+    	try {
+			this.lobbys = Lobby.readLobbyList();
+		} catch (ExceptionBase e) {
+			// TODO show error message
+			e.printStackTrace();
+		}
+    }
+
+	public Lobby[] getLobbys() {
+		return lobbys;
+	}
     
 }
