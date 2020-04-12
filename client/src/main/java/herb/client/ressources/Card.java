d03edcc01ea92ad8d43bbb59317c1ec2fbe405d3
bodybilger <bodybilger@localhost>
2020-04-12 15:28:34 +0200

    added default constructor for json deserialization

--- client/src/main/java/herb/client/ressources/Card.java
+++ client/src/main/java/herb/client/ressources/Card.java
@@ -7,6 +7,11 @@ import herb.client.ressources.core.Trump;
 
 public class Card extends CardBase {
 
+	// default constructor for json deserialization
+	public Card() {
+		super(Suit.Hearts,Rank.Queen,Trump.Hearts);
+	}
+	
 	public Card(Suit suit, Rank rank, Trump trump) {
 		super(suit, rank, trump);
 		// TODO Auto-generated constructor stub
