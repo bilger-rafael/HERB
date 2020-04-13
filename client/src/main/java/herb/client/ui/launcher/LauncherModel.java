55d8a7312853453610396557878245d4a699b558
bodybilger <bodybilger@192.168.0.54>
2020-04-13 15:20:37 +0200

    fixed thread problem

--- client/src/main/java/herb/client/ui/launcher/LauncherModel.java
+++ client/src/main/java/herb/client/ui/launcher/LauncherModel.java
@@ -7,50 +7,52 @@ import herb.client.utils.ServiceLocator;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
-public class LauncherModel extends Model{
+public class LauncherModel extends Model {
 	private ObservableList<Lobby> lobbys = FXCollections.observableArrayList();
 	ServiceLocator servicelocator;
 
+	public LauncherModel() {
+		super();
+		refreshLobbyList();
+		startLobbyUpdater();
+	}
 
-    public LauncherModel() {
-    	super();
-    	refreshLobbyList();
-    	startLobbyUpdater();
-    }
-    
-    public void refreshLobbyList() {
-    	try {
+	public void refreshLobbyList() {
+		try {
 			this.lobbys.clear();
 			this.lobbys.addAll(Lobby.readLobbyList());
 		} catch (ExceptionBase e) {
 			// TODO show error message
 			e.printStackTrace();
 		}
-    }
-    
-    // Create thread to update Lobby periodically
+	}
+
+	// Create thread to update Lobby periodically
 	private void startLobbyUpdater() {
-	Runnable r = new Runnable() {
-		boolean b = true;
-		@Override
-		public void run() {
-			Thread t = new Thread ();
-			t.setDaemon(true);
-			t.start();
-			while (b) {
-				refreshLobbyList();
-				servicelocator.getLogger().info("Launcher refreshed");
-				try {
-					Thread.sleep(10000);
-				} catch (InterruptedException e) {
+		Runnable r = new Runnable() {
+			boolean b = true;
+
+			@Override
+			public void run() {
+				while (b) {
+					refreshLobbyList();
+					servicelocator.getLogger().info("Launcher refreshed");
+					try {
+						Thread.sleep(10000);
+					} catch (InterruptedException e) {
 					}
 				}
 			}
 		};
+
+		Thread t = new Thread(r);
+		t.setDaemon(true);
+		t.start();
+
 	}
-	
+
 	public ObservableList<Lobby> getLobbys() {
 		return lobbys;
 	}
-    
+
 }
