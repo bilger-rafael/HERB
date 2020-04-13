24ea94da8ca4a0a967798522d1126657250f4879
bodybilger <bodybilger@192.168.0.54>
2020-04-13 14:19:39 +0200

    implemented play in Player Service

--- server/src/main/java/herb/server/rest/PlayerController.java
+++ server/src/main/java/herb/server/rest/PlayerController.java
@@ -2,11 +2,16 @@ package herb.server.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import herb.server.Datastore;
+import herb.server.ressources.Card;
 import herb.server.ressources.Player;
 import herb.server.ressources.PlayerNotFoundException;
+import herb.server.ressources.ServerErrorException;
+import herb.server.ressources.core.CardBase;
 import herb.server.ressources.core.ExceptionBase;
 import herb.server.ressources.core.PlayerBase;
 import herb.server.ressources.core.RoundBase;
@@ -27,5 +32,15 @@ public class PlayerController {
 	@GetMapping("/Player({username})/Round")
 	public RoundBase getPlayerRound(@PathVariable String username) throws ExceptionBase {
 		return getPlayer(username).getRound();
+	}
+
+	@PostMapping("/Player({username})/play")
+	public void play(@PathVariable String username, @RequestBody Card card) throws ExceptionBase {
+		CardBase[] cards = getPlayer(username).getHand().getCards();
+
+		for (int i = 0; i < cards.length; i++) {
+			if (cards[i].equals(card))
+				getPlayer(username).play(cards[i]);
+		}
 	}
 }
