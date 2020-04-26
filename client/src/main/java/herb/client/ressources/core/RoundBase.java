c7d99bc06876335146d8c6f3b1276cc3220a7002
bodybilger <bodybilger@localhost>
2020-04-26 16:21:06 +0200

    code formatting

--- client/src/main/java/herb/client/ressources/core/RoundBase.java
+++ client/src/main/java/herb/client/ressources/core/RoundBase.java
@@ -1,38 +1,37 @@
 package herb.client.ressources.core;
 
-import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Map;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //Etter
-public abstract class RoundBase <Player extends PlayerBase, Trick extends TrickBase> {
+public abstract class RoundBase<Player extends PlayerBase, Trick extends TrickBase> {
 	private LinkedList<Trick> tricks = new LinkedList<Trick>();
 	private Trump trump;
 	@JsonIgnoreProperties({ "round", "hand" })
 	private Player[] players;
 	private Integer[] scores;
+	private Player currentStartingPlayer;
 	
-	public RoundBase (Player[] players) {
-		this.players=players;
-	}
-	
-	public void setPlayers(Player[] players) {
+	public RoundBase(Player[] players) {
 		this.players = players;
 	}
 
-	private Player currentStartingPlayer;
+	public void setPlayers(Player[] players) {
+		this.players = players;
+	}
 
 	public void setCurrentStartingPlayer(Player currentStartingPlayer) {
 		this.currentStartingPlayer = currentStartingPlayer;
 	}
 	
-	public LinkedList<Trick> getTricks(){
+	public Player getCurrentStartingPlayer() {
+		return currentStartingPlayer;
+	}
+
+	public LinkedList<Trick> getTricks() {
 		return tricks;
 	}
-	
+
 	public Trump getTrump() {
 		return trump;
 	}
@@ -40,13 +39,9 @@ public abstract class RoundBase <Player extends PlayerBase, Trick extends TrickB
 	public void setTrump(Trump trump) {
 		this.trump = trump;
 	}
-	
+
 	public Player[] getPlayers() {
 		return players;
-	}
-	
-	public Player getCurrentStartingPlayer() {
-		return currentStartingPlayer;
 	}
 
 	public Integer[] getScores() {
@@ -56,5 +51,5 @@ public abstract class RoundBase <Player extends PlayerBase, Trick extends TrickB
 	public void setScores(Integer[] scores) {
 		this.scores = scores;
 	}
-	
+
 }
