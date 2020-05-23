a6a01aa6b3aa939e9baafda7974a5bbcfc7c7571
Anahí <Anah@DESKTOP-VVOSDRT.home>
2020-05-23 14:50:16 +0200

    removed unused code

--- client/src/main/java/herb/client/ui/login/LoginModel.java
+++ client/src/main/java/herb/client/ui/login/LoginModel.java
@@ -5,26 +5,28 @@ import herb.client.ressources.Player;
 import herb.client.ressources.core.ExceptionBase;
 import herb.client.ui.core.Model;
 import herb.client.utils.Datastore;
+
 //Herren
-public class LoginModel extends Model{
+public class LoginModel extends Model {
 	private Player player;
 
-    public LoginModel() {
-    	super();
-    }
-    
-    //if login action 
-    public void login(String username, String password) throws ExceptionBase {
-    	//a "new" login
-    	Login login = new Login(username, password);
-    	//does already a player exist with this username and password
-    	this.player = (Player) login.login();
-    	//does user exist in datastore
-    	Datastore.getInstance().setMainPlayer(this.player);
-    }
-    //if player exist (if player do not exist, throw exception)
+	public LoginModel() {
+		super();
+	}
+
+	// if login action
+	public void login(String username, String password) throws ExceptionBase {
+		// a "new" login
+		Login login = new Login(username, password);
+		// does already a player exist with this username and password
+		this.player = (Player) login.login();
+		// does user exist in datastore
+		Datastore.getInstance().setMainPlayer(this.player);
+	}
+
+	// if player exist (if player do not exist, throw exception)
 	public Player getPlayer() {
 		return player;
 	}
-    
+
 }
